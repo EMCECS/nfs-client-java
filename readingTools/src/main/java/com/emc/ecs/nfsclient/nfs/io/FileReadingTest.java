ffdcaea88fc54515b6a19f2f0c1bf6f0e59c658b
seibed <david.seibert@emc.com>
2018-11-20 11:26:40 -0500

    Updated all copyrights.

--- readingTools/src/main/java/com/emc/ecs/nfsclient/nfs/io/FileReadingTest.java
+++ readingTools/src/main/java/com/emc/ecs/nfsclient/nfs/io/FileReadingTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 EMC Corporation. All Rights Reserved.
+ * Copyright 2017-2018 Dell Inc. or its subsidiaries. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
